974589382abc83746dbc0fceaa365aa6557770f3
Park Seung Ho <dreamhigh924@gmail.com>
2024-09-24 21:48:16 +0900

    chore: 회원 DTO 추가

---
+++ spring/src/main/java/poe/spring/common/Api.java
@@ -0,0 +1,17 @@
+package poe.spring.common;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Api<T> {
+    private T data;
+    private String statusCode;
+    private String resultMessage;
+}
