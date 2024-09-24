d452a8beefe0405bd3ef02a0deb41951fee2a67f
Park Seung Ho <dreamhigh924@gmail.com>
2024-09-24 21:42:04 +0900

    chore: ModelMapper 설정

---
+++ spring/src/main/java/config/ModelMapperConfig.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    // model mapper 설정
+    // DTO -> Entity 변환
+    // Entity -> DTO 변환
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
