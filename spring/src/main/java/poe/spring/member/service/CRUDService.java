4f367573fd2ca78245301cfb151af85520910f71
Park Seung Ho <dreamhigh924@gmail.com>
2024-09-26 19:10:02 +0900

    refactor: 함수 타입 변경 MemberDto를 이름 변경에 의한 반환타입 변경

--- spring/src/main/java/poe/spring/member/service/CRUDService.java
+++ spring/src/main/java/poe/spring/member/service/CRUDService.java
@@ -3,7 +3,8 @@ package poe.spring.member.service;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-import poe.spring.member.dto.MemberDto;
+import poe.spring.member.dto.RequestDto;
+import poe.spring.member.dto.ResponseDto;
 import poe.spring.member.entity.Member;
 import poe.spring.member.entity.MemberRepository;
 
@@ -16,36 +17,35 @@ public class CRUDService {
     private final MemberRepository memberRepository;
     private final ModelMapper modelMapper;
 
-    public void createUser(MemberDto memberDto) {
+    public Long createUser(RequestDto requestDto) {
         // 사용자 생성 로직
-        Member memberEntity = modelMapper.map(memberDto, Member.class);
-        memberRepository.save(memberEntity);
+        Member memberEntity = modelMapper.map(requestDto, Member.class);
+        return memberRepository.save(memberEntity).getId();
     }
 
-    public MemberDto readUser(Long id) {
+    public ResponseDto readUser(Long id) {
         // 사용자 조회 로직
         Member member = memberRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
-        MemberDto memberDto = modelMapper.map(member, MemberDto.class);
-        memberDto.setPassword(null);
-        return memberDto;
+        ResponseDto requestDto = modelMapper.map(member, ResponseDto.class);
+        return requestDto;
     }
 
-    public MemberDto updateUser(MemberDto memberDto) {
+    public ResponseDto updateUser(RequestDto requestDto) {
         // 사용자 수정 로직
-        Member existingMember = memberRepository.findById(memberDto.getId())
+        Member existingMember = memberRepository.findById(requestDto.getId())
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
         // 이메일
-        existingMember.setEmail(Objects.isNull(memberDto.getEmail()) ?
-                existingMember.getEmail() : memberDto.getEmail());
+        existingMember.setEmail(Objects.isNull(requestDto.getEmail()) ?
+                existingMember.getEmail() : requestDto.getEmail());
         // 휴대폰 번호
-        existingMember.setPhoneNumber(Objects.isNull(memberDto.getPhoneNumber()) ?
-                existingMember.getPhoneNumber() : memberDto.getPhoneNumber());
+        existingMember.setPhoneNumber(Objects.isNull(requestDto.getPhoneNumber()) ?
+                existingMember.getPhoneNumber() : requestDto.getPhoneNumber());
         // 비밀번호
-        existingMember.setPassword(Objects.isNull(memberDto.getPassword()) ?
-                existingMember.getPassword() : memberDto.getPassword());
+        existingMember.setPassword(Objects.isNull(requestDto.getPassword()) ?
+                existingMember.getPassword() : requestDto.getPassword());
 
-        return modelMapper.map(memberRepository.save(existingMember), MemberDto.class);
+        return modelMapper.map(memberRepository.save(existingMember), ResponseDto.class);
     }
 
     public void deleteUser(Long id) {
