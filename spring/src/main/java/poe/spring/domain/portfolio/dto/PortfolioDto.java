1b5f40bd53112c7e65794cd097c3ac9eebb1fb44
Park Seung Ho <dreamhigh924@gmail.com>
2024-09-28 12:18:39 +0900

    feat: 포트폴리오 dto 추가

---
+++ spring/src/main/java/poe/spring/domain/portfolio/dto/PortfolioDto.java
@@ -0,0 +1,23 @@
+package poe.spring.domain.portfolio.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PortfolioDto {
+
+    private Long id;
+
+    private String name;
+
+    private Long memberId;
+
+}
