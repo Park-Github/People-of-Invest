1483383f5402a6a23493b27738a05f0693b7ad1d
Park Seung Ho <dreamhigh924@gmail.com>
2024-09-28 12:17:58 +0900

    refactor: map converter 클래스 추가

---
+++ spring/src/main/java/poe/spring/common/MapConverter.java
@@ -0,0 +1,32 @@
+package poe.spring.common;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapConverter<T> {
+
+    public String snakeCaseConverter(String camelCase) {
+        String snakeCase = camelCase
+                .replaceAll("([a-z])([A-Z]+)", "$1_$2")
+                .toLowerCase();
+        return snakeCase;
+    }
+
+    public  Map<String, Object> convertToMap(T dto) {
+        Map<String, Object> data = new HashMap<>();
+        Field[] fields = dto.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                if (field.get(dto) != null)
+                    data.put(snakeCaseConverter(field.getName()), field.get(dto));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return data;
+    }
+
+}
